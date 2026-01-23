d8661cf27841d2fc02bba250689002b41eb69c33
Akash073-hub <akashramachandrabhatbca24@rvu.edu.in>
2026-01-23 14:37:23 +0530

    Finding even number from 0 till 100 using loops and conditional methods this task was given by geeksforgeeks video lecture

---
+++ Loops/Task2.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class Task2 {
+    static void main() {
+        int a = 0;
+        for(;a<100;a++){
+            if(a%2==0){
+                System.out.println(a);
+            }
+        }
+        System.out.println(a);
+    }
+}
