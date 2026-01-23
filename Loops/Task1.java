e906e9b995e7d4bcc43722e07758313b7e49c97b
Akash073-hub <akashramachandrabhatbca24@rvu.edu.in>
2026-01-23 12:35:58 +0530

    Putting a string that user input in loops this task was given by the geeksforgeeks video lecture

---
+++ Loops/Task1.java
@@ -0,0 +1,13 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Task1 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        String var_1 = sc.nextLine();
+        for (int i=0;i < 10;i++){
+            System.out.println(var_1);
+        }
+    }
+}
