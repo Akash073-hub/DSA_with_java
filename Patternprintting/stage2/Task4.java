4580749db4a6e75982fae86a9149c70827f47df2
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-01-29 02:39:25 +0530

    Creating a pattern of triangle by small alphabets using nested loops this task was given by geeksforgeeks video lecture

--- Patternprintting/stage2/Task4.java
+++ Patternprintting/stage2/Task4.java
@@ -1,4 +1,13 @@
 package Patternprintting.stage2;
 
 public class Task4 {
+    static void main() {
+        int var_1= 5;
+        for(int i=0;i<var_1;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print((char)('a'+j));
+            }
+            System.out.println();
+        }
+    }
 }
