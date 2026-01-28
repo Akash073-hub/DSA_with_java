7c3b1d516e99eabf28f514117ce9ad921ae03f19
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-01-28 19:07:49 +0530

    Making square pattern using nested loops this task was given by geeksforgeeks video lecture

--- Patternprintting/Task2.java
+++ Patternprintting/Task2.java
@@ -6,9 +6,8 @@ public class Task2 {
     static void main() {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
-        int columns = sc.nextInt();
         for (int i=1;i<=rows;i++){
-            for (int j=1;j<=columns;j++){
+            for (int j=1;j<=rows;j++){
                 System.out.print("* ");
             }
             System.out.println();
