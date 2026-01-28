5bd7d054705d0fa5f3c266ca66155192648557c3
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-01-28 19:22:42 +0530

    Making square pattern by small case alphabets using nested loops this task was given by geeksforgeeks video lecture

---
+++ Patternprintting/Task5.java
@@ -0,0 +1,16 @@
+package Patternprintting;
+
+import java.util.Scanner;
+
+public class Task5 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        for (int i=1;i<=rows;i++){
+            for (int j=0;j<rows;j++){
+                System.out.print((char) ('a' + j));
+            }
+            System.out.println();
+        }
+    }
+}
