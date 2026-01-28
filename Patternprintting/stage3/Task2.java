03529a3120b97b1178215605c16a5b8da4bada91
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-01-29 03:01:25 +0530

    Creating a pattern of horizontal flipped triangle using nested loops this task was given by geeksforgeeks video lecture

---
+++ Patternprintting/stage3/Task2.java
@@ -0,0 +1,13 @@
+package Patternprintting.stage3;
+
+public class Task2 {
+    static void main() {
+        int var_1 =5;
+        for (int i=var_1;i>=1;i--){
+            for (int j=i;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
