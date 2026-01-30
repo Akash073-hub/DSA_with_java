ea8efe4bcc233be4c4431f9a709299738a76cba2
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-01-30 14:50:22 +0530

    Making a pattern of right angled triangle by using nested loops this task was given by geeksforgeeks video lecture

--- Patternprintting/stage5/Task_1.java
+++ Patternprintting/stage5/Task_1.java
@@ -1,4 +1,19 @@
 package Patternprintting.stage5;
 
+import java.util.Scanner;
+
 public class Task_1 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        for(int i=1;i<=rows;i++) {
+            for (int j = 1; j <=rows-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
