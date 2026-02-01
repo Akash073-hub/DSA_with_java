58f1740534e73b4c7eacf65b3a4b63ca4dd886d1
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-02-01 22:18:52 +0530

    Finding the negative elements in the array using loops this task was given by geeksforgeeks video lecture

--- Arrays/One_dimensional/Array_Basics/Task2.java
+++ Arrays/One_dimensional/Array_Basics/Task2.java
@@ -1,4 +1,18 @@
 package Arrays.One_dimensional.Array_Basics;
 
 public class Task2 {
+    static void main() {
+        int [] arr ={1,-3,-67,2,4,5,-10};
+        System.out.print("[");
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]<0){
+                System.out.print(arr[i]+",");
+            }
+        }
+        System.out.print("]");
+    }
+}
+package Arrays.One_dimensional.Array_Basics;
+
+public class Task2 {
 }
