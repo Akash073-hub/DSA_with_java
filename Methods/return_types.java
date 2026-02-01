768af1ebc976aa90a6e399a8cdedb1bd78b41cd0
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-02-01 15:10:37 +0530

    This user_defined methods are called return types methods that sends a value back to the place where it is called

---
+++ Methods/return_types.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class return_types {
+    static int sum(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = sum(10, 20);
+        System.out.println("Sum = " + result);
+    }
+}
