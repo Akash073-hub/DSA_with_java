7b31bbcf5e9ddd95b765fc4b3ab081b703535d91
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-02-02 20:01:46 +0530

    In this i made a small program that every on ecan understand what enhanced loops are how it works all

---
+++ Enhanced_loops/Task1.java
@@ -0,0 +1,12 @@
+package Enhanced_loops;
+
+public class Task1 {
+    static void main() {
+        int [] arr ={1,2,3,4,6,7};
+        System.out.print("[");
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+        System.out.print("]");
+    }
+}
