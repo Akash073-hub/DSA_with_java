921c39eb115c53bbecfdcdc5d22331f3ae5b1e12
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-02-13 12:27:11 +0530

    This is the another way of bubble sort algoritham

---
+++ SortingAlgorithams/BubbleSorting/Task1_1.java
@@ -0,0 +1,27 @@
+package SortingAlgorithams.BubbleSorting;
+
+import java.util.Arrays;
+
+public class Task1_1 {
+    public static void main(String[] args) {
+        int [] arr ={1,2,3,7,5,4,6};
+        for(int i=0;i<arr.length-1;i++){
+            boolean flag = true;
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true)  break;
+            for (int j=0;j<arr.length-1-i;j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    System.out.println(Arrays.toString(arr));
+                }
+            }
+        }
+    }
+}
