2d75834e1e9f0def8f9d1896509014a16c7be5c2
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-02-16 15:47:36 +0530

    Here i did to move all zeros in arrays at end and in sorting way this using bubble sorting algoritham and using loops and Arrays this task was given by geeksforgeeks video lecture

--- SortingAlgorithams/BubbleSorting/Task3.java
+++ SortingAlgorithams/BubbleSorting/Task3.java
@@ -1,4 +1,19 @@
 package SortingAlgorithams.BubbleSorting;
 
+import java.util.Arrays;
+
 public class Task3 {
+    static void main() {
+        int [] arr = {1,0,4,0,0,5,6,7};
+        int j =0;
+        for(int i=0;i< arr.length;i++){
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
